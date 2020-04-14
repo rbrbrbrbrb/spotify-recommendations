438c0bc821d7b9472ebacd29a73ce8bb95914f8f
RB <rb@Hunter.local>
2020-04-14 16:36:10 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { clientId, scopes } from './env';
+import Home from './Home';
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const redirectUri = 'http://localhost:3000';
 
-function App() {
+const hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+        if (item) {
+            let parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+    }, {});
+
+window.location.hash = "";
+
+function App(props) {
+    const [token, setToken] = useState(null);
+    useEffect(() => {
+        let _token = hash.access_token;
+        if(_token) {
+            setToken(_token);
+        }
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+                {!token && (
+                    <a
+                        className="btn btn--loginApp-link"
+                        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+                    >
+                        Login to Spotify
+                    </a>
+                )}
+                {token && (
+                    <Home />
+                )}
+        </header>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = (props) => {
+    return (
+        <div>placeholder</div>
+    );
+}
+
+export default Home;

7e953e93fc540b6f409ecbe307bf467c18fece83
RB <rb@Hunter.local>
2020-04-18 21:18:50 +0800

    WIP basic layout and state management

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,23 @@
 import React, {useState, useEffect} from 'react';
 
+const initialParamsState = {
+    min_valence: 0,
+    max_valence: 1,
+    min_danceability: 0,
+    max_danceability: 1,
+    min_energy: 0,
+    max_energy: 1,
+    min_popularity: 0,
+    max_popularity: 100,
+    min_instrumentalness: 0,
+    max_instrumentalness: 1,
+    min_liveness: 0,
+    max_liveness: 1
+};
 
 const Home = (props) => {
-    const [genres, setGenres] = useState([]);
+    const [genrepool, setGenrePool] = useState([]);
+    const [filter_params, setFilterParams] = useState(initialParamsState);
 
     useEffect(() => {
         //test spotify api call
@@ -18,12 +33,44 @@ const Home = (props) => {
         .then(res => res.json())
         .then(json => {
             console.log(json.genres);
-            setGenres(json.genres);
+            setGenrePool(json.genres);
         });
     }, [props.token]);
 
+    const onChangeParam = (key, val) => {
+        setFilterParams({
+            ...filter_params,
+            [key]: val
+        });
+    }
     return (
-        <div>placeholder</div>
+        <div className="container">
+            <h2>Get Spotify recommendation based on...</h2>
+            <div className="selectors-row">
+                <div className="selector valence">
+                    <h4>Valence</h4>
+                    <p>
+                        A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
+                    </p>
+                    <div>
+                        Minimum:
+                            <input
+                                type="textbox"
+                                className="number-input"
+                                value={filter_params.min_valence}
+                                onChange={(e) => onChangeParam('min_valence', e.target.value)}
+                            />
+                    </div>
+                    <div>
+                    Maximum:  <input type="textbox" />
+                    </div>
+                </div>
+                <div className="selector danceablility">
+                    Minimum:  <input type="textbox" />
+                    Maximum:  <input type="textbox" />
+                </div>
+            </div>
+        </div>
     );
 }
 
